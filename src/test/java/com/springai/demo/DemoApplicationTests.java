7c115b17e15b0ba3263d2ac8637ceaddafedc18e
Aziz <aziz.zina2001@gmail.com>
2024-04-29 16:24:27 +0100

    Initial commit

---
+++ src/test/java/com/springai/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
